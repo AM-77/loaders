52bbb1efa06edf733489b817c60f0a1a8c33b1df
AM-77 <amine.griche77@protonmail.ch>
2020-11-01 15:20:26 +0100

    add Loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+type props = {
+  id: number;
+  html: string;
+};
+
+const Loader: React.FC<props> = ({ id, html }) => {
+  return (
+    <div
+      className={`loader loader-${id}`}
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+  );
+};
+
+Loader.propTypes = {
+  id: PropTypes.number.isRequired,
+  html: PropTypes.string.isRequired,
+};
+
+export default Loader;
